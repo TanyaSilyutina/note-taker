1221cd8f9c98e08fcfafa351109298d03a607532
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-06-14 20:57:09 -0700

    added crud functionality

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ const staticOptions = {
 
 const notes_db = require('./db/db.json');
 
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // get notes returns HTML
-app.get('/notes', (req, res) => res.sendFile('public/notes.html', staticOptions))
-// get * returns index
-app.get('/*', (req, res) => res.sendFile('public/index.html', staticOptions))
-
+app.get('/notes', (req, res) => res.sendFile('/public/notes.html', staticOptions));
 
 app.get('/api/notes', (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
@@ -40,7 +36,7 @@ app.post('/api/notes', (req, res) => {
         const notes = JSON.parse(data);
         notes.push(note);
 
-        fs.writeFile("./db/db.json", JSON.stringify(notes,null,4),(err)=>{
+        fs.writeFile("./db/db.json", JSON.stringify(notes,null,4),(err)=> {
             if (err) {
                 return res.status(500).json({
                     msg:"error writing to db"
@@ -50,7 +46,26 @@ app.post('/api/notes', (req, res) => {
             }
         })
     });
+});
+
+app.delete('/api/notes/:id', function(req, res) {
+    notes_db.splice(req.params.id, 1);
+    update_db(notes_db);
+    res.json(notes_db);
 })
 
+function update_db(notes) {
+    fs.writeFile('db/db.json', JSON.stringify(notes, null), (err) => {
+        if (err) throw err;
+        return true;
+    })}
+
+app.get('/api/notes/:id', function(req, res) {
+    res.json(notes_db[req.params.id]);
+})
+
+
+// get * returns index
+app.get('/*', (req, res) => res.sendFile('/public/index.html', staticOptions))
 
 app.listen(port, () => console.log(`The port ${port} is connected`));
